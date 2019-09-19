2e3a439f42b1d0c26cb0de4464f3dfe665253de9
github username <github email address>
2019-09-18 22:44:58 -0400

    mergeAttempt1 try 1

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// console.log("NONONO")
 const yodaTestSentence = 'Create a perimeter around the survivors'
 
 const splitWords = yodaTestSentence.split(' ');
@@ -5,8 +6,7 @@ const splitWords = yodaTestSentence.split(' ');
 
 
 console.log(splitWords)
-const testURL = `https://dictionaryapi.com/api/v3/references/collegiate/json/${splitWords[0]}?key=82e0af33-78ba-4492-bdbd-30f85423f7be`;
-
+const testURL = ""
 function partOfSpeechDetector(word) {
   
 }
